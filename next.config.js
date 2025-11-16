d3a0466852467ed47fceb1a65f39f236312ea27a
kishanmypmssolutions <kishanmypmssolutions@gmail.com>
2025-11-16 22:43:46 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  api: {
-    bodyParser: false
-  }
+  // If you need bodyParser:false for specific API routes, remove this and set config per API file.
+  // Next.js no longer accepts `api` top-level config in next.config.js
 }
+
 module.exports = nextConfig
